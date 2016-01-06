6c11991b658fba443e49ed525b43712baf0cb59d
Tai LE <lhtai181@yahoo.com>
2016-01-06 17:12:21 +0700

    update side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,17 +1,18 @@
 <div id='cssmenu'>
 	<ul>
-		<li {{ (Request::is('*sale') ? 'class=active' : '') }} ><a
-			href="{{ url('sale') }}"
-			class=" " data-drop="magazine"><i class="glyphicon glyphicon-cog"></i>
-				Opportunity </a>
+		<li {{ (Request::is('*sale') ? 'class=active' : '') }}><a href="{{ url('sale') }}" data-drop="magazine">
+                <i class="glyphicon glyphicon-cog"></i> &nbsp;Opportunity</a>
 		</li>
 
-		<li class='has-sub {{ (Request::is(' *value*') ? 'openactive' : '') }}' >
-		<a href='#' class="dropdown-toggle" data-drop="magazine"><i class="glyphicon glyphicon-th"></i>&nbspValue</a>
-			<ul
-				style="display: {{ (Request::is('*value*') ? 'block' : 'none') }}">
-				<li><a href='/value/full'><span class="sub-menu"><i class="glyphicon glyphicon-align-justify"></i>&nbspFull Value</a></span></li>
-				<li><a href="/value/weighted" class="dropdown-toggle"><span class="sub-menu"><i class="glyphicon glyphicon-align-left"></i>&nbspWeighted Value</a></span></li>
+		<li class='has-sub {{ (Request::is(' *value*') ? 'openactive' : '') }}'>
+		    <a href='#' class="dropdown-toggle" data-drop="magazine"><i class="glyphicon glyphicon-th"></i> &nbsp;Value</a>
+			<ul style="display: {{ (Request::is('*value*') ? 'block' : 'none') }}">
+				<li>
+                    <a href='/value/full'><span class="sub-menu"><i class="glyphicon glyphicon-align-justify"></i> &nbsp; Full Value</span></a>
+                </li>
+				<li>
+                    <a href="/value/weighted" class="dropdown-toggle"><span class="sub-menu"><i class="glyphicon glyphicon-align-left"></i> &nbsp; Weighted Value</span></a>
+                </li>
 			</ul>
 		</li>
 
