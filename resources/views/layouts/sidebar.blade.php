bd0f0c6555d009908f8844c39e78f23de617d1e4
nghiep.to <nghiep.to@infonam.com>
2016-01-06 14:49:16 +0700

    add css menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,8 +10,8 @@
 		<a href='#' class="glyphicon glyphicon-th dropdown-toggle " data-drop="magazine">&nbspValue</a>
 			<ul
 				style="display: {{ (Request::is('*value*') ? 'block' : 'none') }}">
-				<li><a href='/value/full'><span aria-hidden="true" class="glyphicon glyphicon-align-justify"></span><span>&nbspFull Value</span></a></li>
-				<li><a href="/value/weighted" class="dropdown-toggle"><span aria-hidden="true" class="glyphicon glyphicon-align-left"></span>&nbspWeighted Value</a></li>
+				<li><a href='/value/full'><span aria-hidden="true" class="glyphicon glyphicon-align-justify"></span><span class="sub-menu">&nbspFull Value</span></a></li>
+				<li><a href="/value/weighted" class="dropdown-toggle"><span aria-hidden="true" class="glyphicon glyphicon-align-left"></span><span class="sub-menu">&nbspWeighted Value</span></a></li>
 			</ul>
 		</li>
 
