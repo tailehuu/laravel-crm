7aabd1b3fa0cd9e9fd070536c5cde5503ceec8b2
nghiep.to <nghiep.to@infonam.com>
2016-01-06 15:10:59 +0700

    add icon sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,16 +2,16 @@
 	<ul>
 		<li {{ (Request::is('*sale') ? 'class=active' : '') }} ><a
 			href="{{ url('sale') }}"
-			class=" glyphicon glyphicon-cog" data-drop="magazine">
+			class=" " data-drop="magazine"><i class="glyphicon glyphicon-cog"></i>
 				Opportunity </a>
 		</li>
 
 		<li class='has-sub {{ (Request::is(' *value*') ? 'openactive' : '') }}' >
-		<a href='#' class="glyphicon glyphicon-th dropdown-toggle " data-drop="magazine">&nbspValue</a>
+		<a href='#' class="dropdown-toggle" data-drop="magazine"><i class="glyphicon glyphicon-th"></i>&nbspValue</a>
 			<ul
 				style="display: {{ (Request::is('*value*') ? 'block' : 'none') }}">
-				<li><a href='/value/full'><span aria-hidden="true" class="glyphicon glyphicon-align-justify"></span><span class="sub-menu">&nbspFull Value</span></a></li>
-				<li><a href="/value/weighted" class="dropdown-toggle"><span aria-hidden="true" class="glyphicon glyphicon-align-left"></span><span class="sub-menu">&nbspWeighted Value</span></a></li>
+				<li><a href='/value/full'><i class="glyphicon glyphicon-align-justify"></i><span class="sub-menu">Full Value</span></a></li>
+				<li><a href="/value/weighted" class="dropdown-toggle"><i class="glyphicon glyphicon-align-left"></i><span class="sub-menu">&nbspWeighted Value</span></a></li>
 			</ul>
 		</li>
 
